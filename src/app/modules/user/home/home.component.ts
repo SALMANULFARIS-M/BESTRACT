009ceae916999ccf0e751adeb549b4d9108bd079
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-04-26 15:44:58 +0530

    changed animation of the logos display

--- src/app/modules/user/home/home.component.ts
+++ src/app/modules/user/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { HeaderComponent } from '../../../shared/user/header/header.component';
 import { heroAnimations, zoomSlide } from '../../../shared/constants/animation';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
@@ -8,12 +8,12 @@ import { PartnerLogo } from '../../../shared/constants/logo.interface';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent,FooterComponent, CommonModule,FloatingSocialComponent],
+  imports: [HeaderComponent, FooterComponent, CommonModule, FloatingSocialComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [heroAnimations,zoomSlide]
+  animations: [heroAnimations, zoomSlide]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   isAnimating = true;
   images = [
     { src: 'https://imageio.forbes.com/specials-images/imageserve/673b3fa4d94f770224891222/Happy-young-business-team-planning-strategy-while-working-at-the-modern-office/960x0.jpg?format=jpg&width=960' },
@@ -21,12 +21,13 @@ export class HomeComponent implements OnInit {
     { src: 'https://images.pexels.com/photos/8761345/pexels-photo-8761345.jpeg?cs=srgb&dl=pexels-pavel-danilyuk-8761345.jpg&fm=jpg' },
     { src: 'https://images.pexels.com/photos/2608517/pexels-photo-2608517.jpeg?cs=srgb&dl=pexels-bertellifotografia-2608517.jpg&fm=jpg' },
   ];
-  marqueeSpeed = 40; // Base speed in seconds
   animationState: string[] = [];
   private currentIndex = 0;
+  visibleLogos: any[] = [];
+  currentLogo = 0;
+  intervalId: any;
 
-
-   logos: PartnerLogo[] = [
+  logos: PartnerLogo[] = [
     { name: 'Castle', imageUrl: 'https://bestract.com/user/partners/Castle.png' },
     { name: 'IZee Bschool', imageUrl: 'https://bestract.com/user/partners/IZee%20Bschool%20Logo.png' },
     { name: 'Iaeknir2', imageUrl: 'https://bestract.com/user/partners/Iaeknir2.jpg' },
@@ -36,7 +37,10 @@ export class HomeComponent implements OnInit {
     { name: 'OFCJNS', imageUrl: 'https://bestract.com/user/partners/OFCJNS.jpg' },
     { name: 'Study in Bengaluru', imageUrl: 'https://studyinbengaluru.com/sib-violet.png' },
     { name: 'Soofi Mandhi', imageUrl: 'https://bestract.com/user/partners/SoofiMandhi.jpg' },
-    { name: 'Subz', imageUrl: 'https://bestract.com/user/partners/Subz.png' }
+    { name: 'Subz', imageUrl: 'https://bestract.com/user/partners/Subz.png' },
+    { name: 'Team Lease', imageUrl: 'https://d3isa0ssinyrxx.cloudfront.net/teamlease-content/2020/09/01/TeamleaseLogo_1598946933-1598946934-53943.jpeg' },
+    { name: 'First Source', imageUrl: 'https://www.firstsource.com/themes/custom/first_source/images/Firstsource-logo.svg' },
+
   ];
 
   constructor(@Inject(PLATFORM_ID) private platformId: any) { }
@@ -47,7 +51,12 @@ export class HomeComponent implements OnInit {
       this.animationState = this.images.map((_, i) => i === 0 ? 'in' : 'out');
       // Start the slideshow
       this.startSlideshow();
+      this.updateVisibleLogos();
+      this.intervalId = setInterval(() => {
+        this.nextLogos();
+      }, 3000);
     }
+
   }
 
   startSlideshow() {
@@ -65,8 +74,22 @@ export class HomeComponent implements OnInit {
   onAnimationDone() {
     this.isAnimating = false;
   }
-  trackByFn(index: number, item: PartnerLogo): string {
-    return item.imageUrl;
+
+  updateVisibleLogos() {
+    this.visibleLogos = this.logos.slice(this.currentLogo, this.currentLogo + 4);
+  }
+
+  nextLogos() {
+    this.currentLogo += 4;
+    if (this.currentLogo >= this.logos.length) {
+      this.currentLogo = 0;
+    }
+    this.updateVisibleLogos();
+  }
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 
 }
